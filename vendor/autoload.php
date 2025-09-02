58523648a4140e4a8d3c50a61a7f2f518f0d0c80
S.Sharkova <sharkova-2000@mail.ru>
2025-09-02 17:51:56 +0300

    edit autoload.php

--- vendor/autoload.php
+++ vendor/autoload.php
@@ -1,1 +1,14 @@
 <?php
+
+function _autoload($className)
+{
+	$fileName = strtolower($className) . ".php";
+
+	//$file =;
+	
+	if (file_exists($file) == false) {
+		return false;
+	}
+
+	include($file);
+}
