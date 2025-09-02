35ff1df457a5fe17ad6353bf2fc43d6d570cd834
S.Sharkova <sharkova-2000@mail.ru>
2025-09-02 17:15:49 +0300

    create Task class

---
+++ app/Models/Task.php
@@ -0,0 +1,15 @@
+<?php
+
+class Task
+{
+	public $title;
+	public $description;
+	private $user_id;
+	private $date_create;
+	private $date_finished;
+
+	/*public function getTasks($user_id)
+	{
+
+	}*/
+}
