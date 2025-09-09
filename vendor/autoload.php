c337ed68de2fa674d39ea5fe7eef591b2c6f5704
S.Sharkova <sharkova-2000@mail.ru>
2025-09-09 20:13:40 +0300

    edit old code in autoload.php

--- vendor/autoload.php
+++ vendor/autoload.php
@@ -2,14 +2,17 @@
 
 function autoloadClasses($className)
 {
-	$path = __DIR__ . "../";
-	$file = $path . $className . ".php";
-	
+	$path = __DIR__ . DIRECTORY_SEPARATOR . ".." . DIRECTORY_SEPARATOR;
+	$file = $path . str_replace("App", "app", str_replace("\\", DIRECTORY_SEPARATOR, $className)) . ".php";
+
 	if (file_exists($file) == false) {
-		return false;
+		echo "да!";
+		//return false;
+	} else {
+		echo "нет";
 	}
 
-	require_once $file;
+	//require_once $file;
 }
-
+autoloadClasses("App\Controllers\LoginController");
 spl_autoload_register("autoloadClasses");
