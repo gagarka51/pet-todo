3b421f63ef1869057af0e869846a0fc7a1ac25bf
S.Sharkova <sharkova-2000@mail.ru>
2025-09-12 19:26:40 +0300

    delete old code and create birthday template

---
+++ resources/templates/birthday.php
@@ -0,0 +1,17 @@
+<div class="container">
+	<div class="toast-container position-fixed bottom-0 end-0 p-3">
+		<div id="liveToast" class="toast" role="alert" aria-live="assertive" aria-atomic="true">
+			<div class="toast-header d-flex justify-content-between">
+				<strong class="text-primary">ToDo</strong>
+				<small><?=date("Y-m-d") ?></small>
+				<button type="button" class="btn-close" data-bs-dismiss="toast" aria-label="Закрыть"></button>
+			</div>
+			<div class="toast-body">
+				<p>
+					От всей души поздравляем вас с днём рождения! Желаем вам здоровья крепкого, удачи, успехов в поставленных задачах. Пусть сбываются ваши мечты.
+				</p>
+				<p>С уважением команда ToDo!</p>
+			</div>
+		</div>
+	</div>
+</div>
