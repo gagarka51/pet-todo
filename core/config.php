311aa13f7d7d422912e90a886543d4376f60ac01
S.Sharkova <sharkova-2000@mail.ru>
2025-09-13 13:36:09 +0300

    del old code and add class Config

---
+++ core/config.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Core;
+
+class Config 
+{
+	const BASE_URL = "/";
+	const DEFAULT_CONTROLLER = "HomeController";
+    const DEFAULT_ACTION = "index";
+    //const DEBUG_MODE = true;
+
+    /*public static function setDebugMode(bool $debug) {
+        ini_set('display_errors', $debug ? 'On' : 'Off');
+        error_reporting($debug ? E_ALL : 0);
+    }*/
+}
