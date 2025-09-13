a8b976aaf445b480e3921023b25dea0b96e3b215
S.Sharkova <sharkova-2000@mail.ru>
2025-09-13 13:48:38 +0300

    edit index.php

--- public/index.php
+++ public/index.php
@@ -1,19 +1,13 @@
 <?php
 
-require_once "../config/config.php";
+session_start();
+require_once "../app/Core/Config.php";
+require_once "../app/Core/Router.php";
 require __DIR__ . "/../vendor/autoload.php";
 
-$request = explode('/', trim($_SERVER['REQUEST_URI'], '/'));
-
-$controllerName = isset($request[0]) ? ucfirst($request[0]) . 'Controller' : 'HomeController';
-$actionMethod = isset($request[1]) ? strtolower($request[1]) : 'index';
-
-$controllerFilePath = CONTROLLER_PATH . $controllerName . '.php';
-if (file_exists($controllerFilePath)) {
-	include_once $controllerFilePath;
-	$controllerInstance = new $controllerName();
-	call_user_func([$controllerInstance, $actionMethod]);
-} else {
-	header("HTTP/1.0 404 Not Found");
-	die("Ошибка 404: Контроллер не найден.");
+try {
+	$router = new Core\\Router();
+	$router->dispatch($_SERVER["REQUEST_URI"]);
+} catch (\\Throwable $th) {
+	echo '<pre>' . print_r($th->getMessage(), true) . '</pre>';
 }
