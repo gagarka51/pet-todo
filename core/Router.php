a4b471d59689f89c75d80fe2ba16c9c4a1a18ea0
S.Sharkova <sharkova-2000@mail.ru>
2025-09-13 13:40:26 +0300

    create Router

---
+++ core/Router.php
@@ -0,0 +1,23 @@
+<?php 
+
+namespace Core;
+
+use Core\Config;
+
+class Router
+{
+	public function dispatch(string $uri) {
+		list($controllerName, $method) = explode("/", ltrim($uri, "/"));
+
+		$controllerClass = empty($controllerName) ? Config::DEFAULT_CONTROLLER : ucfirst($controllerName) . "Controller";
+		$method = empty($method) ? Config::DEFAULT_ACTION : $method;
+
+		if (!file_exists("../app/controllers/" . $controllerClass . ".php")) {
+			throw new Exception("Контроллер {$controllerClass} не найден!");
+		}
+
+		require_once "../app/controllers/" . $controllerClass . ".php";
+		$controller = new $controllerClass();
+		$controller->$method();
+	}
+}
