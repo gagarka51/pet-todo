c89bdaf4aa596ec314fc89b997368ca36e134de9
S.Sharkova <sharkova-2000@mail.ru>
2025-09-13 13:23:12 +0300

    create file config.php and add constants for app

---
+++ config/config.php
@@ -0,0 +1,9 @@
+<?php
+
+define('BASE_URL', '');
+define('ROOT_DIR', dirname(__FILE__) . DIRECTORY_SEPARATOR);
+define('APP_DIR', ROOT_DIR . 'app' . DIRECTORY_SEPARATOR);
+define('PUBLIC_DIR', ROOT_DIR . 'public' . DIRECTORY_SEPARATOR);
+define('VIEWS_DIR', APP_DIR . 'Views' . DIRECTORY_SEPARATOR);
+define('MODELS_DIR', APP_DIR . 'Models' . DIRECTORY_SEPARATOR);
+define('CONTROLLERS_DIR', APP_DIR . 'Controllers' . DIRECTORY_SEPARATOR);
