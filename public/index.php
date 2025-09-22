912297aada529cb141284af0f2b9b081036c3045
S.Sharkova <sharkova-2000@mail.ru>
2025-09-22 18:06:34 +0300

    moved files in Settings folder

--- public/index.php
+++ public/index.php
@@ -4,15 +4,14 @@ session_start();
 
 require_once __DIR__ . "/../vendor/autoload.php";
 
-use App\Core\Config;
-use App\Core\Router;
+use App\Core\{Config, Db, Router};
 use App\Controllers\HomeController;
 
 $controller = new HomeController();
 $controller->index();
 
-/*$router = new Router();
-$router->dispatch($_SERVER["REQUEST_URI"]);*/
+$router = new Router();
+$router->dispatch($_SERVER["REQUEST_URI"]);
 
 /*try {
 	$router = new Router();
