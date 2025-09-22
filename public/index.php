fbf84bd6c56bbf466722f803c158aa370e226b2b
S.Sharkova <sharkova-2000@mail.ru>
2025-09-22 17:36:25 +0300

    edit index.php

--- public/index.php
+++ public/index.php
@@ -12,9 +12,9 @@ $controller = new HomeController();
 $controller->index();
 
 
-/*try {
+try {
 	$router = new Router();
 	$router->dispatch($_SERVER["REQUEST_URI"]);
 } catch (\Throwable $th) {
 	echo '<pre>' . print_r($th->getMessage(), true) . '</pre>';
-}*/
+}
