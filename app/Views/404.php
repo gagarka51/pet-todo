c17da5bcd83b0e75a1bb24a120fde62b247143b8
S.Sharkova <sharkova-2000@mail.ru>
2025-09-26 20:16:12 +0300

    create page 404

---
+++ app/Views/404.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Not found</title>
+</head>
+<body>
+	<main>Эта страница была похищена конкурентами...</main>
+</body>
+</html>
